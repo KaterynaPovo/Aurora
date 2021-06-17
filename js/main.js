4aa1badc7a3e175cf7659098909b07c043864ac1
KaterynaPovo <k.povoliaieva@gmail.com>
2021-06-17 17:06:26 +0300

    Add me project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     var topOfDivScroll = $(".intro").offset().top - $(window).height();
+  
+//     $(window).scroll(function() {
+//       if ($(window).scrollTop() > topOfDivScroll) {
+//         $(".intro").hide();
+//       } else $(".header-nav").show();
+//     });
+//   });
